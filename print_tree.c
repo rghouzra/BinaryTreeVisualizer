85b9abb09b3a628d4e9e7d6f7e6221d29b5c3dcd
reda ghouzraf <98324229+rghouzra@users.noreply.github.com>
2023-07-03 16:58:32 +0100

    Update print_tree.c

--- print_tree.c
+++ print_tree.c
@@ -18,8 +18,9 @@ void print_in_dot(t_tree *node,int i, int *count, FILE *fp)
         return;
     int node_id = ++(*count);
     fprintf(fp, "  node%d [label=\"%s \"];\n", node_id, (char *)node->value);
-    if (i != -1)
+    if (i != -1){
         fprintf(fp, "  node%d -> node%d;\n",i,node_id);
+    }
     print_in_dot(node->left,node_id,count, fp);
     print_in_dot(node->right,node_id,count, fp);
 }
@@ -33,8 +34,7 @@ void print_tree(t_tree *root)
      * u can override fp with fopen system function like in this example:
      * fp = fopen("filename", "w");
     */
-    if(fp == nullptr)
-    {
+    if (fp == nullptr){
     	fprintf(stderr,"%s\n", strerror(errno));
 	exit(1);
     }
